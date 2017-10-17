c608bec3c4b497b5714d2a43c1113076439bb296
Krista <krista.t.jekel@gmail.com>
2017-10-17 15:31:39 -0600

    Print data to console

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,7 +1,44 @@
-if ("geolocation" in navigator){
-      navigator.geolocation.getCurrentPosition(function(position) {
-      console.log(position.coords.latitude, position.coords.longitude);
-    });
-}else{
-	console.log("Can't find your location");
-}
+//get location of user
+  $(document).ready(function() {
+
+  var longitude = "";
+  var latitude = "";
+  var url = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
+
+  function getLocation(){
+    if ("geolocation" in navigator){
+          navigator.geolocation.getCurrentPosition(function(position) {
+          //console.log(position.coords.latitude, position.coords.longitude);
+          latitude = position.coords.latitude;
+          longitude = position.coords.longitude
+        });
+    }else{
+    	console.log("Can't find your location");
+    }
+  }
+
+  getLocation();
+
+
+
+
+$.getJSON(url, function(data) {
+    console.log(data);
+});
+
+
+
+
+
+
+
+
+
+});
+//$("#tweet").on("click", function(){
+  //     
+    //var qString = $(".quote").html(quote);
+		//window.open(url);
+		//console.log(tweet);
+    //});
+
