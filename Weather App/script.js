9ead4fbf0b166e18c13a4b1822d4af1c04dcb7d4
Krista <krista.t.jekel@gmail.com>
2017-10-18 15:30:55 -0600

    change C to F one click

--- Weather App/script.js
+++ Weather App/script.js
@@ -4,6 +4,8 @@
   var longitude = "";
   var latitude = "";
   var url = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
+  var temp = "";
+  
 
   //Get location
   function getLocation(){
@@ -26,34 +28,41 @@ function getData(){
     //document.write(data.main.temp);
     temp = data.main.temp;
     weather = data.weather[0].main;
+    icon = data.weather[0].icon;
 
     $(".temp").html(temp);
     $(".weather").html(weather);
 
     //$("#icon").attr('src', icon);
-    //document.getElementById("icon").src= icon;
+    //document.getElementById("icon").src= url;
+      var num = $(".temp").html();
+      
 
-  
+    far = Math.floor((num * 9) / 5 + 32);
+    $(".temperature-text").html(num + " °F");
+
+    $("#tweet").click(function () {
+      //makes text change
+       var text = $('#tweet').text();
+       $(this).text(text == "°F" ? "°C" : "°F");
+       $(".temp").html(far);
+    });
   });
 }
 getData();
 
-
-// toggle button text
-   $("#tweet").click(function () {
-       var text = $('#tweet').text();
-       $(this).text(text == "°F" ? "°C" : "°F");
-    });
-
-
-
-
+  
+  
+//
 
 });
 
+//temperature conversion
 
 //Fahrenheit to Celsius : (°F − 32) ÷ 1.8 = °C 
 //Celsius to Fahrenheit : (°C × 1.8) + 32 = °F
+
+//open data in new window
 //$("#tweet").on("click", function(){
   //     
     //var qString = $(".quote").html(quote);
