78e86f7475e44550571355fbee1eceab3003ddb8
Krista <krista.t.jekel@gmail.com>
2017-10-18 09:45:24 -0600

    temp weather show in div

--- Weather App/script.js
+++ Weather App/script.js
@@ -5,6 +5,7 @@
   var latitude = "";
   var url = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
 
+  //Get location
   function getLocation(){
     if ("geolocation" in navigator){
           navigator.geolocation.getCurrentPosition(function(position) {
@@ -16,25 +17,43 @@
     	console.log("Can't find your location");
     }
   }
-
   getLocation();
 
 
+//get data and post to page
+function getData(){
+  $.getJSON(url, function(data) {
+    //document.write(data.main.temp);
+    temp = data.main.temp;
+    weather = data.weather[0].main;
+
+    //console.log(we);
+    $(".temp").html(temp);
+    $(".weather").html(weather);
+    //$(".weather").html(JSON.stringify(json));
+  });
+}
+getData();
 
 
-$.getJSON(url, function(data) {
-    console.log(data);
+
+
+
+
+
+
 });
 
 
 
 
+// $("#tweet").on("click", function(){
+  //    // Only change code below this line.
+    //  $.getJSON(url, function(json) {
+  //$(".weather").html(JSON.stringify(json));
+//});
 
 
-
-
-
-});
 //$("#tweet").on("click", function(){
   //     
     //var qString = $(".quote").html(quote);
